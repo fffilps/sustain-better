532da8ff2b7e31664e8a7a0691d827c6039e4b8d
Nyi Htet <nyihtet4dev@icloud.com>
2024-06-30 09:53:31 -0700

    recycle updated

--- src/app/dashboard/earn_task/page.tsx
+++ src/app/dashboard/earn_task/page.tsx
@@ -4,65 +4,65 @@ import Image from "next/image";
 export default function Page() {
   return (
     <div className="flex flex-col p-10 gap-20">
-    {/* Could use array structure by mapping thru each items, if dataset for tasks is ready */}
-    {/* first task  */}
-    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-      <a href="#">
-        <Image
-          className="rounded-t-lg"
-          src="/docs/images/blog/image-1.jpg"
-          width={200}
-          height={200}
-          alt=""
-        />
-      </a>
-      <div className="p-5">
+      {/* Could use array structure by mapping thru each items, if dataset for tasks is ready */}
+      {/* first task  */}
+      <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Noteworthy technology acquisitions 2021
-          </h5>
+          <Image
+            className="rounded-t-lg"
+            src="/docs/images/blog/image-1.jpg"
+            width={200}
+            height={200}
+            alt=""
+          />
         </a>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          Here are the biggest enterprise technology acquisitions of 2021 so
-          far, in reverse chronological order.
-        </p>
-        <a
-          href="#"
-          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Earn Task
-        </a>
-      </div>
+        <div className="p-5">
+          <a href="#">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Noteworthy technology acquisitions 2021
+            </h5>
+          </a>
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order.
+          </p>
+          <a
+            href="#"
+            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Earn Task
+          </a>
+        </div>
       </div>
 
       {/* second task */}
       <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-      <a href="#">
-        <Image
-          className="rounded-t-lg"
-          src="/docs/images/blog/image-1.jpg"
-          width={200}
-          height={200}
-          alt=""
-        />
-      </a>
-      <div className="p-5">
         <a href="#">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Noteworthy technology acquisitions 2021
-          </h5>
+          <Image
+            className="rounded-t-lg"
+            src="/docs/images/blog/image-1.jpg"
+            width={200}
+            height={200}
+            alt=""
+          />
         </a>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          Here are the biggest enterprise technology acquisitions of 2021 so
-          far, in reverse chronological order.
-        </p>
-        <a
-          href="#"
-          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Earn Task
-        </a>
-      </div>
+        <div className="p-5">
+          <a href="#">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              Noteworthy technology acquisitions 2021
+            </h5>
+          </a>
+          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order.
+          </p>
+          <a
+            href="#"
+            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Earn Task
+          </a>
+        </div>
       </div>
     </div>
   );
--- src/app/dashboard/recycle/page.tsx
+++ src/app/dashboard/recycle/page.tsx
@@ -3,21 +3,30 @@ import { Button } from "@vechain/dapp-kit-ui";
 
 export default function Page() {
   return (
-    <div className="flex flex-col gap-20">
+    <div className="flex flex-col gap-20 p-20 items-center">
       <div className="flex flex-row gap-20">
-        <div className="flex flex-col">
-        <p>Trash Item</p>
-        <CameraButton/>
+        {/* trash item capturing region */}
+        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 text-white p-8">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Trash Item
+          </h5>
+          <CameraButton />
         </div>
-        
-        <div className="flex flex-col">
-        <p>Trash Bin</p>
-        <CameraButton/>
+
+        {/* trash bin capturing region */}
+        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 text-white p-8">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Trash Bin
+          </h5>
+          <CameraButton />
         </div>
       </div>
+
+      {/* button for image processing and detection using AI/tensorflow
+      implement onClick() function */}
       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded w-1/5">
-      Generate
-    </button>
+        Generate
+      </button>
     </div>
-  )
+  );
 }
